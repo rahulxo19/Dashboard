9d59cbccba84676a8eb0cb6ca3e841c1527e81e2
rahulxo19 <rahulchhikara2001@gmail.com>
2023-01-17 22:19:47 +0530

    react Dashboard

---
+++ src/data/dummy.js
@@ -0,0 +1,815 @@
+import React from 'react';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { FiShoppingBag, FiBarChart, FiCreditCard } from 'react-icons/fi';
+import { BsBoxSeam, BsCurrencyDollar, BsShield } from 'react-icons/bs';
+import { IoMdContacts } from 'react-icons/io';
+import { RiContactsLine } from 'react-icons/ri';
+import { MdOutlineSupervisorAccount } from 'react-icons/md';
+import { HiOutlineRefresh } from 'react-icons/hi';
+import { TiTick } from 'react-icons/ti';
+import { GrLocation } from 'react-icons/gr';
+import avatar from './avatar.jpg';
+import avatar2 from './avatar2.jpg';
+import avatar3 from './avatar3.png';
+import avatar4 from './avatar4.jpg';
+import product4 from './product4.jpg';
+import product5 from './product5.jpg';
+import product6 from './product6.jpg';
+import product7 from './product7.jpg';
+
+export const gridOrderImage = (props) => (
+  <div>
+    <img
+      className="rounded-xl h-20 md:ml-3"
+      src={props.ProductImage}
+      alt="order-item"
+    />
+  </div>
+);
+
+export const gridOrderStatus = (props) => (
+  <button
+    type="button"
+    style={{ background: props.StatusBg }}
+    className="text-white py-1 px-2 capitalize rounded-2xl text-md"
+  >
+    {props.Status}
+  </button>
+);
+
+const gridEmployeeProfile = (props) => (
+  <div className="flex items-center gap-2">
+    <img
+      className="rounded-full w-10 h-10"
+      src={props.EmployeeImage}
+      alt="employee"
+    />
+    <p>{props.Name}</p>
+  </div>
+);
+
+const gridEmployeeCountry = (props) => (
+  <div className="flex items-center justify-center gap-2">
+    <GrLocation />
+    <span>{props.Country}</span>
+  </div>
+);
+const customerGridImage = (props) => (
+  <div className="image flex gap-4">
+    <img
+      className="rounded-full w-10 h-10"
+      src={props.CustomerImage}
+      alt="employee"
+    />
+    <div>
+      <p>{props.CustomerName}</p>
+      <p>{props.CustomerEmail}</p>
+    </div>
+  </div>
+);
+
+const customerGridStatus = (props) => (
+  <div className="flex gap-2 justify-center items-center text-gray-700 capitalize">
+    <p style={{ background: props.StatusBg }} className="rounded-full h-3 w-3" />
+    <p>{props.Status}</p>
+  </div>
+);
+
+export const barPrimaryXAxis = {
+  valueType: 'Category',
+  interval: 1,
+  majorGridLines: { width: 0 },
+};
+export const barPrimaryYAxis = {
+  majorGridLines: { width: 0 },
+  majorTickLines: { width: 0 },
+  lineStyle: { width: 0 },
+  labelStyle: { color: 'transparent' },
+};
+
+export const FinancialPrimaryXAxis = {
+  valueType: 'DateTime',
+  minimum: new Date('2016, 12, 31'),
+  maximum: new Date('2017, 9, 30'),
+  crosshairTooltip: { enable: true },
+  majorGridLines: { width: 0 },
+};
+
+export const FinancialPrimaryYAxis = {
+  title: 'Price',
+  minimum: 100,
+  maximum: 180,
+  interval: 20,
+  lineStyle: { width: 0 },
+  majorTickLines: { width: 0 },
+};
+
+export const LinePrimaryXAxis = {
+  valueType: 'DateTime',
+  labelFormat: 'y',
+  intervalType: 'Years',
+  edgeLabelPlacement: 'Shift',
+  majorGridLines: { width: 0 },
+  background: 'white',
+};
+
+export const LinePrimaryYAxis = {
+  labelFormat: '{value}%',
+  rangePadding: 'None',
+  minimum: 0,
+  maximum: 100,
+  interval: 20,
+  lineStyle: { width: 0 },
+  majorTickLines: { width: 0 },
+  minorTickLines: { width: 0 },
+};
+
+export const customersGrid = [
+  { type: 'checkbox', width: '50' },
+  { headerText: 'Name',
+    width: '150',
+    template: customerGridImage,
+    textAlign: 'Center' },
+  { field: 'ProjectName',
+    headerText: 'Project Name',
+    width: '150',
+    textAlign: 'Center' },
+  { field: 'Status',
+    headerText: 'Status',
+    width: '130',
+    format: 'yMd',
+    textAlign: 'Center',
+    template: customerGridStatus },
+  {
+    field: 'Weeks',
+    headerText: 'Weeks',
+    width: '100',
+    format: 'C2',
+    textAlign: 'Center' },
+  { field: 'Budget',
+    headerText: 'Budget',
+    width: '100',
+    format: 'yMd',
+    textAlign: 'Center' },
+
+  { field: 'Location',
+    headerText: 'Location',
+    width: '150',
+    textAlign: 'Center' },
+
+  { field: 'CustomerID',
+    headerText: 'Customer ID',
+    width: '120',
+    textAlign: 'Center',
+    isPrimaryKey: true,
+  },
+
+];
+
+export const employeesGrid = [
+  { headerText: 'Employee',
+    width: '150',
+    template: gridEmployeeProfile,
+    textAlign: 'Center' },
+  { field: 'Name',
+    headerText: '',
+    width: '0',
+    textAlign: 'Center',
+  },
+  { field: 'Title',
+    headerText: 'Designation',
+    width: '170',
+    textAlign: 'Center',
+  },
+  { headerText: 'Country',
+    width: '120',
+    textAlign: 'Center',
+    template: gridEmployeeCountry },
+
+  { field: 'HireDate',
+    headerText: 'Hire Date',
+    width: '135',
+    format: 'yMd',
+    textAlign: 'Center' },
+
+  { field: 'ReportsTo',
+    headerText: 'Reports To',
+    width: '120',
+    textAlign: 'Center' },
+  { field: 'EmployeeID',
+    headerText: 'Employee ID',
+    width: '125',
+    textAlign: 'Center' },
+];
+
+export const links = [
+  {
+    title: 'Dashboard',
+    links: [
+      {
+        name: 'ecommerce',
+        icon: <FiShoppingBag />,
+      },
+    ],
+  },
+
+  {
+    title: 'Pages',
+    links: [
+      {
+        name: 'orders',
+        icon: <AiOutlineShoppingCart />,
+      },
+      {
+        name: 'employees',
+        icon: <IoMdContacts />,
+      },
+      {
+        name: 'customers',
+        icon: <RiContactsLine />,
+      },
+    ],
+  },
+];
+
+export const cartData = [
+  {
+    image:
+      product5,
+    name: 'butterscotch ice-cream',
+    category: 'Milk product',
+    price: '$250',
+  },
+  {
+    image:
+      product6,
+    name: 'Supreme fresh tomato',
+    category: 'Vegetable Item',
+    price: '$450',
+  },
+  {
+    image:
+      product7,
+    name: 'Red color candy',
+    category: 'Food Item',
+    price: '$190',
+  },
+];
+
+export const chatData = [
+  {
+    image:
+      avatar2,
+    message: 'Roman Joined the Team!',
+    desc: 'Congratulate him',
+    time: '9:08 AM',
+  },
+  {
+    image:
+      avatar3,
+    message: 'New message received',
+    desc: 'Salma sent you new message',
+    time: '11:56 AM',
+  },
+  {
+    image:
+      avatar4,
+    message: 'New Payment received',
+    desc: 'Check your earnings',
+    time: '4:39 AM',
+  },
+  {
+    image:
+      avatar,
+    message: 'Jolly completed tasks',
+    desc: 'Assign her new tasks',
+    time: '1:12 AM',
+  },
+];
+
+export const earningData = [
+  {
+    icon: <MdOutlineSupervisorAccount />,
+    amount: '39,354',
+    percentage: '-4%',
+    title: 'Customers',
+    iconColor: '#03C9D7',
+    iconBg: '#E5FAFB',
+    pcColor: 'red-600',
+  },
+  {
+    icon: <BsBoxSeam />,
+    amount: '4,396',
+    percentage: '+23%',
+    title: 'Products',
+    iconColor: 'rgb(255, 244, 229)',
+    iconBg: 'rgb(254, 201, 15)',
+    pcColor: 'green-600',
+  },
+  {
+    icon: <FiBarChart />,
+    amount: '423,39',
+    percentage: '+38%',
+    title: 'Sales',
+    iconColor: 'rgb(228, 106, 118)',
+    iconBg: 'rgb(255, 244, 229)',
+
+    pcColor: 'green-600',
+  },
+  {
+    icon: <HiOutlineRefresh />,
+    amount: '39,354',
+    percentage: '-12%',
+    title: 'Refunds',
+    iconColor: 'rgb(0, 194, 146)',
+    iconBg: 'rgb(235, 250, 242)',
+    pcColor: 'red-600',
+  },
+];
+
+export const recentTransactions = [
+  {
+    icon: <BsCurrencyDollar />,
+    amount: '+$350',
+    title: 'Paypal Transfer',
+    desc: 'Money Added',
+    iconColor: '#03C9D7',
+    iconBg: '#E5FAFB',
+    pcColor: 'green-600',
+  },
+  {
+    icon: <BsShield />,
+    amount: '-$560',
+    desc: 'Bill Payment',
+    title: 'Wallet',
+    iconColor: 'rgb(0, 194, 146)',
+    iconBg: 'rgb(235, 250, 242)',
+    pcColor: 'red-600',
+  },
+  {
+    icon: <FiCreditCard />,
+    amount: '+$350',
+    title: 'Credit Card',
+    desc: 'Money reversed',
+    iconColor: 'rgb(255, 244, 229)',
+    iconBg: 'rgb(254, 201, 15)',
+
+    pcColor: 'green-600',
+  },
+  {
+    icon: <TiTick />,
+    amount: '+$350',
+    title: 'Bank Transfer',
+    desc: 'Money Added',
+
+    iconColor: 'rgb(228, 106, 118)',
+    iconBg: 'rgb(255, 244, 229)',
+    pcColor: 'green-600',
+  },
+  {
+    icon: <BsCurrencyDollar />,
+    amount: '-$50',
+    percentage: '+38%',
+    title: 'Refund',
+    desc: 'Payment Sent',
+    iconColor: '#03C9D7',
+    iconBg: '#E5FAFB',
+    pcColor: 'red-600',
+  },
+];
+
+export const productsPerformance = [
+  {
+    image:
+      product5,
+    title: 'Is it good butterscotch ice-cream?',
+    desc: 'Ice-Cream, Milk, Powder',
+    rating: 'Good',
+    itemSold: '65%',
+    earningAmount: '$546,000',
+  },
+  {
+    image:
+      product6,
+    title: 'Supreme fresh tomato available',
+    desc: 'Market, Mall',
+    rating: 'Excellent',
+    itemSold: '98%',
+    earningAmount: '$780,000',
+  },
+  {
+    image:
+      product7,
+    title: 'Red color candy from Gucci',
+    desc: 'Chocolate, Yummy',
+    rating: 'Average',
+    itemSold: '46%',
+    earningAmount: '$457,000',
+  },
+  {
+    image:
+      product4,
+    title: 'Stylish night lamp for night',
+    desc: 'Electric, Wire, Current',
+    rating: 'Poor',
+    itemSold: '23%',
+    earningAmount: '$123,000',
+  },
+];
+
+export const themeColors = [
+  {
+    name: 'blue-theme',
+    color: '#1A97F5',
+  },
+  {
+    name: 'green-theme',
+    color: '#03C9D7',
+  },
+  {
+    name: 'purple-theme',
+    color: '#7352FF',
+  },
+  {
+    name: 'red-theme',
+    color: '#FF5C8E',
+  },
+  {
+    name: 'indigo-theme',
+    color: '#1E4DB7',
+  },
+  {
+    color: '#FB9678',
+    name: 'orange-theme',
+  },
+];
+
+export const userProfileData = [
+  {
+    icon: <BsCurrencyDollar />,
+    title: 'My Profile',
+    desc: 'Account Settings',
+    iconColor: '#03C9D7',
+    iconBg: '#E5FAFB',
+  },
+  {
+    icon: <BsShield />,
+    title: 'My Inbox',
+    desc: 'Messages & Emails',
+    iconColor: 'rgb(0, 194, 146)',
+    iconBg: 'rgb(235, 250, 242)',
+  },
+  {
+    icon: <FiCreditCard />,
+    title: 'My Tasks',
+    desc: 'To-do and Daily Tasks',
+    iconColor: 'rgb(255, 244, 229)',
+    iconBg: 'rgb(254, 201, 15)',
+  },
+];
+
+export const ordersGrid = [
+  {
+    headerText: 'Image',
+    template: gridOrderImage,
+    textAlign: 'Center',
+    width: '120',
+  },
+  {
+    field: 'OrderItems',
+    headerText: 'Item',
+    width: '150',
+    editType: 'dropdownedit',
+    textAlign: 'Center',
+  },
+  { field: 'CustomerName',
+    headerText: 'Customer Name',
+    width: '150',
+    textAlign: 'Center',
+  },
+  {
+    field: 'TotalAmount',
+    headerText: 'Total Amount',
+    format: 'C2',
+    textAlign: 'Center',
+    editType: 'numericedit',
+    width: '150',
+  },
+  {
+    headerText: 'Status',
+    template: gridOrderStatus,
+    field: 'OrderItems',
+    textAlign: 'Center',
+    width: '120',
+  },
+  {
+    field: 'OrderID',
+    headerText: 'Order ID',
+    width: '120',
+    textAlign: 'Center',
+  },
+
+  {
+    field: 'Location',
+    headerText: 'Location',
+    width: '150',
+    textAlign: 'Center',
+  },
+];
+
+export const customersData = [
+  {
+    CustomerID: 1001,
+    CustomerName: 'Nirav Joshi',
+    CustomerEmail: 'nirav@gmail.com',
+    CustomerImage:
+      avatar2,
+    ProjectName: 'Hosting Press HTML',
+    Status: 'Active',
+    StatusBg: '#8BE78B',
+    Weeks: '40',
+    Budget: '$2.4k',
+    Location: 'India',
+  },
+  {
+    CustomerID: 1002,
+
+    CustomerName: 'Sunil Joshi',
+    CustomerEmail: 'sunil@gmail.com',
+    ProjectName: 'Elite Admin',
+    Status: 'Active',
+    CustomerImage:
+      avatar3,
+
+    StatusBg: '#8BE78B',
+    Weeks: '11',
+    Budget: '$3.9k',
+    Location: 'India',
+  },
+  {
+    CustomerID: 1003,
+
+    CustomerName: 'Andrew McDownland',
+    CustomerEmail: 'andrew@gmail.com',
+    ProjectName: 'Real Homes WP Theme',
+    Status: 'Pending',
+    CustomerImage:
+      avatar4,
+    StatusBg: '#FEC90F',
+    Weeks: '19',
+    Budget: '$24.5k',
+    Location: 'USA',
+  },
+];
+
+export const employeesData = [
+  {
+    EmployeeID: 1,
+    Name: 'Nancy Davolio',
+    Title: 'Sales Representative',
+    HireDate: '01/02/2021',
+    Country: 'USA',
+    ReportsTo: 'Carson',
+    EmployeeImage:
+    avatar3,
+  },
+  {
+    EmployeeID: 2,
+    Name: 'Nasimiyu Danai',
+    Title: 'Marketing Head',
+    HireDate: '01/02/2021',
+    Country: 'USA',
+    ReportsTo: 'Carson',
+    EmployeeImage:
+      avatar3,
+  },
+  {
+    EmployeeID: 3,
+    Name: 'Iulia Albu',
+    Title: 'HR',
+    HireDate: '01/02/2021',
+    Country: 'USA',
+    ReportsTo: 'Carson',
+    EmployeeImage:
+      avatar4,
+  },
+];
+
+export const ordersData = [
+  {
+    OrderID: 10248,
+    CustomerName: 'Vinet',
+
+    TotalAmount: 32.38,
+    OrderItems: 'Fresh Tomato',
+    Location: 'USA',
+    Status: 'pending',
+    StatusBg: '#FB9678',
+    ProductImage:
+      product6,
+  },
+  {
+    OrderID: 345653,
+    CustomerName: 'Carson Darrin',
+    TotalAmount: 56.34,
+    OrderItems: 'Butter Scotch',
+    Location: 'Delhi',
+    Status: 'complete',
+    StatusBg: '#8BE78B',
+    ProductImage:
+      product5,
+  },
+  {
+    OrderID: 390457,
+    CustomerName: 'Fran Perez',
+    TotalAmount: 93.31,
+    OrderItems: 'Candy Gucci',
+    Location: 'New York',
+    Status: 'active',
+    StatusBg: '#03C9D7',
+    ProductImage:
+      product7,
+  },
+];
+
+export const lineChartData = [
+  [
+    { x: new Date(2005, 0, 1), y: 21 },
+    { x: new Date(2006, 0, 1), y: 24 },
+    { x: new Date(2007, 0, 1), y: 36 },
+    { x: new Date(2008, 0, 1), y: 38 },
+    { x: new Date(2009, 0, 1), y: 54 },
+    { x: new Date(2010, 0, 1), y: 57 },
+    { x: new Date(2011, 0, 1), y: 70 },
+  ],
+  [
+    { x: new Date(2005, 0, 1), y: 28 },
+    { x: new Date(2006, 0, 1), y: 44 },
+    { x: new Date(2007, 0, 1), y: 48 },
+    { x: new Date(2008, 0, 1), y: 50 },
+    { x: new Date(2009, 0, 1), y: 66 },
+    { x: new Date(2010, 0, 1), y: 78 },
+    { x: new Date(2011, 0, 1), y: 84 },
+  ],
+
+  [
+    { x: new Date(2005, 0, 1), y: 10 },
+    { x: new Date(2006, 0, 1), y: 20 },
+    { x: new Date(2007, 0, 1), y: 30 },
+    { x: new Date(2008, 0, 1), y: 39 },
+    { x: new Date(2009, 0, 1), y: 50 },
+    { x: new Date(2010, 0, 1), y: 70 },
+    { x: new Date(2011, 0, 1), y: 100 },
+  ],
+];
+export const dropdownData = [
+  {
+    Id: '1',
+    Time: 'March 2021',
+  },
+  {
+    Id: '2',
+    Time: 'April 2021',
+  }, {
+    Id: '3',
+    Time: 'May 2021',
+  },
+];
+export const SparklineAreaData = [
+  { x: 1, yval: 2 },
+  { x: 2, yval: 6 },
+  { x: 3, yval: 8 },
+  { x: 4, yval: 5 },
+  { x: 5, yval: 10 },
+
+];
+
+export const lineCustomSeries = [
+  { dataSource: lineChartData[0],
+    xName: 'x',
+    yName: 'y',
+    name: 'Germany',
+    width: '2',
+    marker: { visible: true, width: 10, height: 10 },
+    type: 'Line' },
+
+  { dataSource: lineChartData[1],
+    xName: 'x',
+    yName: 'y',
+    name: 'England',
+    width: '2',
+    marker: { visible: true, width: 10, height: 10 },
+    type: 'Line' },
+
+  { dataSource: lineChartData[2],
+    xName: 'x',
+    yName: 'y',
+    name: 'India',
+    width: '2',
+    marker: { visible: true, width: 10, height: 10 },
+    type: 'Line' },
+
+];
+
+export const pieChartData = [
+  { x: 'Labour', y: 18, text: '18%' },
+  { x: 'Legal', y: 8, text: '8%' },
+  { x: 'Production', y: 15, text: '15%' },
+  { x: 'License', y: 11, text: '11%' },
+  { x: 'Facilities', y: 18, text: '18%' },
+  { x: 'Taxes', y: 14, text: '14%' },
+  { x: 'Insurance', y: 16, text: '16%' },
+];
+
+export const contextMenuItems = [
+  'AutoFit',
+  'AutoFitAll',
+  'SortAscending',
+  'SortDescending',
+  'Copy',
+  'Edit',
+  'Delete',
+  'Save',
+  'Cancel',
+  'PdfExport',
+  'ExcelExport',
+  'CsvExport',
+  'FirstPage',
+  'PrevPage',
+  'LastPage',
+  'NextPage',
+];
+
+export const ecomPieChartData = [
+  { x: '2018', y: 18, text: '35%' },
+  { x: '2019', y: 18, text: '15%' },
+  { x: '2020', y: 18, text: '25%' },
+  { x: '2021', y: 18, text: '25%' },
+];
+
+export const stackedChartData = [
+  [
+    { x: 'Jan', y: 111.1 },
+    { x: 'Feb', y: 127.3 },
+    { x: 'Mar', y: 143.4 },
+    { x: 'Apr', y: 159.9 },
+    { x: 'May', y: 159.9 },
+    { x: 'Jun', y: 159.9 },
+    { x: 'July', y: 159.9 },
+  ],
+  [
+    { x: 'Jan', y: 111.1 },
+    { x: 'Feb', y: 127.3 },
+    { x: 'Mar', y: 143.4 },
+    { x: 'Apr', y: 159.9 },
+    { x: 'May', y: 159.9 },
+    { x: 'Jun', y: 159.9 },
+    { x: 'July', y: 159.9 },
+  ],
+];
+
+export const stackedCustomSeries = [
+
+  { dataSource: stackedChartData[0],
+    xName: 'x',
+    yName: 'y',
+    name: 'Budget',
+    type: 'StackingColumn',
+    background: 'blue',
+
+  },
+
+  { dataSource: stackedChartData[1],
+    xName: 'x',
+    yName: 'y',
+    name: 'Expense',
+    type: 'StackingColumn',
+    background: 'red',
+
+  },
+
+];
+
+export const stackedPrimaryXAxis = {
+  majorGridLines: { width: 0 },
+  minorGridLines: { width: 0 },
+  majorTickLines: { width: 0 },
+  minorTickLines: { width: 0 },
+  interval: 1,
+  lineStyle: { width: 0 },
+  labelIntersectAction: 'Rotate45',
+  valueType: 'Category',
+};
+
+export const stackedPrimaryYAxis = {
+  lineStyle: { width: 0 },
+  minimum: 100,
+  maximum: 400,
+  interval: 100,
+  majorTickLines: { width: 0 },
+  majorGridLines: { width: 1 },
+  minorGridLines: { width: 1 },
+  minorTickLines: { width: 0 },
+  labelFormat: '{value}',
+};
+
